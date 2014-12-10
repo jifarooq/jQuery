ac154f1eaad0c38795e083afbb64776a4503d1df
Justin Farooq <justinfarooq@gmail.com>
2014-12-10 13:59:20 -0800

    multiple apples

--- w6d3-snake/Snake/js/snake-view.js
+++ w6d3-snake/Snake/js/snake-view.js
@@ -3,16 +3,16 @@
 	if (typeof Game === "undefined") { window.Game = {}; }
 
 	var View = Game.View = function (el) {
-		this.stepSize = 100;
 	  this.$el = el;
 	  this.board = new Game.Board();
+		this.stepSize = 100;
 
-	  this.interval = window.setInterval(
-	  	this.step.bind(this), 
-	  	this.stepSize
-  	);
+	  this.playGame();
 
 	  $(window).on("keydown", this.handleKeyEvent.bind(this));
+	  $(window).on("keydown", this.handlePausePlay.bind(this));
+	  $('.pause').on("click", this.stopGame.bind(this));
+	  $('.play').on("click", this.playGame.bind(this));
 	}
 
 	KEYS = {
@@ -28,7 +28,23 @@
 	  }
 	}
 
-	View.prototype.render = function () {
+	View.prototype.handlePausePlay = function (event) {
+		if (event.keyCode === 32) this.stopGame();  // spacebar
+		if (event.keyCode === 13) this.playGame();	// enter
+	}
+
+	View.prototype.stopGame = function() {
+		window.clearInterval(this.interval);
+	};
+
+	View.prototype.playGame = function() {
+		this.interval = window.setInterval(
+	  	this.step.bind(this), 
+	  	this.stepSize
+  	);
+	};
+
+	View.prototype.render = function() {
 		var view = this, board = view.board;
 		var matrix = buildMatrix();
 
@@ -36,7 +52,10 @@
 			matrix[ seg[0] ][ seg[1] ].addClass('snake');
 		});
 
-		matrix[board.apple.pos[0]][board.apple.pos[1]].addClass("apple");
+		_(board.apples).each(function(apple) {
+			matrix[apple.pos[0]][apple.pos[1]].addClass("apple");
+		});
+
 		this.$el.empty();
 
 		_(matrix).each(function(row) {
@@ -64,13 +83,20 @@
 	  }
   };
 
-	View.prototype.step = function () {
+  View.prototype.renderStats = function() {
+  	var score = this.board.snake.applesEaten * 10;
+  	content = '<p>Your score: ' + score + '</p>';
+  	$('.stats').html(content);
+  }
+
+	View.prototype.step = function() {
 		if (this.board.snake.segments.length > 0) {
 			this.board.snake.move();
 			this.render();
+			this.renderStats();
 		} else {
 			alert("You lose!");
-			window.clearInterval(this.interval);
+			this.stopGame();
 		}
 	}
 
--- w6d3-snake/Snake/js/snake.js
+++ w6d3-snake/Snake/js/snake.js
@@ -1,3 +1,5 @@
+// replace apples on interval?
+
 (function () {
 
   if (typeof Game === "undefined") {
@@ -6,31 +8,30 @@
 
   // SNAKE CLASS
   var Snake = Game.Snake = function(board) {
+    this.applesEaten = 0;
     this.board = board;
     this.dir = "N";
-    this.segments = [[9, 9]];
-    this.addSegment(); this.addSegment();
-    this.addSegment(); this.addSegment();
+    this.segments = [[ board.dim - 1, board.dim - 1 ]];
   };
 
-  Snake.prototype.eat = function () {
+  Snake.prototype.eat = function (apple) {
     var head = this.segments[0];
-    return _matchedPositions(head, this.board.apple.pos);
-
+    return _matchedPositions(head, apple.pos);
   };
     
   Snake.prototype.move = function() {
     this.addSegment();
     this.segments = this.segments.slice(0, -1);
 
-    if ( this.eat() ) {
-      this.board.apple.replace();
-      this.addSegment();
-      this.addSegment();
-    }
-
+    _(this.board.apples).each(function(apple) {
+      if (this.eat(apple)) {
+        apple.replace();
+        this.addSegment();
+        this.applesEaten++;
+      }
+    }.bind(this));
+      
     if ( !this.isValid() ) {
-      // debugger
       this.segments = [];   // destroy snake if invalid
     }
   };
@@ -56,11 +57,24 @@
   }
 
   Snake.prototype.turn = function(newDir) {
-    this.dir = newDir;
+    if (this.segments.length > 1 && _oppDirections([newDir, this.dir])) 
+      return;
+    else
+      this.dir = newDir;
   };
 
   function _matchedPositions(pos1, pos2) {
     return pos1[0] === pos2[0] && pos1[1] === pos2[1];
+  }
+
+  function _oppDirections(dirPair) {
+    var oppCases = [['N', 'S'], ['E', 'W'], ['W', 'E'], ['S', 'N']];
+
+    for (var i = 0; i < oppCases.length; i++) {
+      if ( _matchedPositions(dirPair, oppCases[i]) ) return true;
+    }
+
+    return false;
   }
 
 
@@ -114,10 +128,15 @@
   var Board = Game.Board = function() {
     this.dim = DIM;
     this.snake = new Snake(this);
-    this.apple = new Apple(this);
+    this.apples = [];
+
+    _.times(NUM_APPLES, function() {
+      this.apples.push(new Apple(this));
+    }.bind(this));
   };
 
   DIM = 20;
+  NUM_APPLES = 5;
 
   Board.prototype.validPos = function() {
     var head = this.snake.segments[0];
